a8f18c10586b139345bbe926c55d990ecd6ba185
Gokoverse <w5vrvyc5m8@privaterelay.appleid.com>
2026-06-20 02:04:43 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,41 @@
+﻿window.gokoverseData = {
+  latestVideoUrl: "",
+  producks: [
+    {
+      title: "Gokoverse",
+      type: "Website",
+      status: "Live",
+      description:
+        "Gokoverse linkleri, uygulamalari, videolari ve dijital calismalari icin ana merkez.",
+      image: "gokoverse-visual.svg",
+      website: "https://www.gokoverse.com",
+      appStore: "",
+      playStore: "",
+      video: "",
+    },
+    {
+      title: "First App",
+      type: "Mobile app",
+      status: "Soon",
+      description:
+        "App Store ve Play Store linkleri hazir oldugunda bu karttan yayinlanacak.",
+      image: "",
+      website: "",
+      appStore: "",
+      playStore: "",
+      video: "",
+    },
+    {
+      title: "Video Works",
+      type: "YouTube",
+      status: "Soon",
+      description:
+        "YouTube videolari ve tanitim icerikleri eklendikce burada listelenecek.",
+      image: "",
+      website: "",
+      appStore: "",
+      playStore: "",
+      video: "",
+    },
+  ],
+};
